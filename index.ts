81995645ce8ff700676da8f6d3155d52987f66fa
MrDaPoyo <wiiuforever25@gmail.com>
2025-03-27 21:36:01 +0100

    a

--- index.ts
+++ index.ts
@@ -4,11 +4,18 @@ console.log("IndieSearch scraper running.");
 
 db.scrapedURL("https://thinliquid.dev/buttons-galore", db.hash("https://thinliquid.dev/buttons-galore"));
 
+let currentlyScraping = [] as any;
+
 const urlsToScrape = await db.retrieveURLsToScrape();
 if (Array.isArray(urlsToScrape)) {
     for (const url of urlsToScrape) {
+      if (!currentlyScraping.includes(url)) {
+        continue;
+      } else { 
+        currentlyScraping.push(url);
         const scraperWorker = new Worker("scrapeWebsite.ts");
         scraperWorker.postMessage(url);
+      }
     }
 }
 console.log("Finished!");
--- scrapeWebsite.ts
+++ scrapeWebsite.ts
@@ -5,6 +5,7 @@ import * as bcrypt from "bcryptjs";
 declare var self: Worker;
 
 function scrapeEntireWebsite(url: string) {
+    console.log("Scraping: " + url);
     puppeteer.launch({headless: 'shell'}).then(async (browser: any) => {
         const page = await browser.newPage();
         await page.goto(url);
@@ -29,21 +30,3 @@ self.onmessage = (event: MessageEvent) => {
         scrapeEntireWebsite(event.data.url);
     }
 };
-
-async function scrapeWebsite(url: string) {
-    console.log("Scraping", url);
-    if (!url.startsWith("http://") && !url.startsWith("https://")) {
-        url = "https://" + url;
-    }
-
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    try {
-        await page.goto(url, { waitUntil: "domcontentloaded", timeout: 30000 });
-    }
-    catch (error) {
-        console.error("Failed to load page", url);
-        return;
-    }
-}
